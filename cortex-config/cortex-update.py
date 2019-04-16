e95eff05d55768d1d8a5c3c93c11eacb30ebc12d
xiongyu96 <49020513+xiongyu96@users.noreply.github.com>
2019-04-16 15:37:41 +0800

    update cortexnode once

---
+++ cortex-config/cortex-update.py
@@ -0,0 +1,75 @@
+#!/usr/bin/python
+import urllib2, json, time
+from subprocess import PIPE, Popen
+import threading
+import re
+
+version = 'CortexAutoUpdate 1.0.0'
+RefreshScriptInterval = 3600
+
+def set_interval(func, sec):
+    def func_wrapper():
+        set_interval(func, sec)
+        func()
+    t = threading.Timer(sec, func_wrapper)
+    t.start()
+    return t
+
+def ge(v1, v2):
+    v1 = re.split('\.', v1)
+    v2 = re.split('\.', v2)
+
+    v1 = [int(v1[i]) for i in range(len(v1))]
+    v2 = [int(v2[i]) for i in range(len(v2))]
+
+    if(v1 >= v2):
+        return True
+    else:
+        return False     
+
+def sh(command):
+    p = Popen(command, stderr=PIPE, stdout=PIPE, shell=True)
+    ret, err = p.communicate()
+    if err != None:
+        raise Exception(err)
+    else:
+        return ret
+
+def load_config(path):
+    with open(path, 'r') as raw_config:
+        config = json.load(raw_config)
+    return config
+
+def save_config(config):
+    with open('/opt/cortex/config.json', 'w') as f:
+        f.write(json.dumps(config))
+        f.close()
+
+def update():
+    try:
+        sh('wget -q https://raw.githubusercontent.com/lizhencortex/cortex-deploy/dev/config.json -O /opt/cortex/update.json')
+        update = load_config('/opt/cortex/update.json')
+        config = load_config('/opt/cortex/config.json')
+
+        node_config = config.get('cortexnode', None)
+        if node_config != None:
+            if node_config['autoupdate'] == "enable" and ge(update['cortexnode']['version'], node_config['version']) :
+                sh('wget -q https://raw.githubusercontent.com/lizhencortex/cortex-deploy/xy/cortex-config/cortex.sh -O /opt/cortex/cortex.sh.new')
+                sh('mv /opt/cortex/cortex.sh.new /opt/cortex/cortex.sh')
+                sh('supervisorctl restart cortexnode')
+                config['cortexnode']['version'] = update['cortexnode']['version']
+
+        minerpool_config = config.get('minerpool', None)
+        if minerpool_config != None:
+            pass
+
+        miner_config = config.get('miner', None)
+        if miner_config != None:
+            pass
+        
+        save_config(config)
+    except BaseException:
+        pass
+
+if __name__ == '__main__':
+    set_interval(update, RefreshScriptInterval)
