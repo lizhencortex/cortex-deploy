565c8866c4484d887f41e5db55963416ca9d576e
zhen <zhen@cortexlabs.ai>
2019-06-26 17:35:00 +0800

    update

--- deploy.sh
+++ deploy.sh
@@ -42,68 +42,16 @@ deploy() {
         exit 1
     fi
 
-    if [ -n $CUDA101 ]; then
-        echo CUDA10.1 detected
-        if [ "$1" != "install" ]; then
-            wget https://raw.githubusercontent.com/CortexFoundation/Cortex_Release/dev/cortex-core/bin/cortex-cuda10.1 -O cortex-cuda10.1
-        fi
-        DOWNLOAD_STATUS=$(ls | grep cortex-cuda10.1)
-        if [ -z $DOWNLOAD_STATUS ]; then
-            echo download failed
-            exit 1
-        fi
-        mv cortex-cuda10.1 cortex
-    else
-        if [ -n $CUDA10 ]; then
-            echo CUDA10.0 detected
-            if [ "$1" != "install" ]; then
-                wget https://raw.githubusercontent.com/CortexFoundation/Cortex_Release/dev/cortex-core/bin/cortex-cuda10.0 -O cortex-cuda10.0
-            fi
-            DOWNLOAD_STATUS=$(ls | grep cortex-cuda10.0)
-            if [ -z $DOWNLOAD_STATUS ]; then
-                echo download failed
-                exit 1
-            fi
-            mv cortex-cuda10.0 cortex
-        #    wget https://raw.githubusercontent.com/lizhencortex/MonitorServer/master/static/miner-cuda10.0
-        #    mv miner-cuda10.0 cuda_miner
-        else
-            if [ -n $CUDA92 ]; then
-                echo CUDA9.2 detected
-                if [ "$1" != "install" ]; then
-                    wget https://raw.githubusercontent.com/CortexFoundation/Cortex_Release/dev/cortex-core/bin/cortex-cuda9.2 -O cortex-cuda9.2
-                fi
-                DOWNLOAD_STATUS=$(ls | grep cortex-cuda9.2)
-                if [ -z $DOWNLOAD_STATUS ]; then
-                    echo download failed
-                    exit 1
-                fi
-                mv cortex-cuda9.2 cortex
-            else
-                echo CUDA9.0 detected
-                if [ "$1" != "install" ]; then
-                    wget https://raw.githubusercontent.com/CortexFoundation/Cortex_Release/dev/cortex-core/bin/cortex-cuda9.0 -O cortex-cuda9.0
-                fi
-                DOWNLOAD_STATUS=$(ls | grep cortex-cuda9.0)
-                if [ -z $DOWNLOAD_STATUS ]; then
-                    echo download failed
-                    exit 1
-                fi
-                mv cortex-cuda9.0 cortex
-            fi
-        fi
-    fi
-    chmod +x cortex
-
+    echo CUDA detected
     if [ -n "$1" ]; then
-        DPLOY_PATH="$1" 
+        DPLOY_PATH="$1"
     fi
 
     rm -rf $DPLOY_PATH
     mkdir -p $DPLOY_PATH
     chmod 777 $DPLOY_PATH -R
     mkdir -p $DPLOY_PATH/logs
-    wget https://codeload.github.com/lizhencortex/cortex-deploy/zip/master -O cortex-package.zip
+    wget https://codeload.github.com/lizhencortex/cortex-deploy/zip/dev -O cortex-package.zip
     DOWNLOAD_STATUS=$(ls | grep cortex-package.zip)
     if [ -z $DOWNLOAD_STATUS ]; then
         echo download failed
@@ -112,13 +60,13 @@ deploy() {
 
     unzip cortex-package.zip
     mv cortex-deploy-master cortex-package
-    mv ./cortex-package/cortex-config/* $DPLOY_PATH/
-    mv cortex $DPLOY_PATH/
-    #mv cuda_miner $DPLOY_PATH/
-    chmod +x ./cortex-package/service/cortex-monitor.sh
+    chmod +x ./cortex-package/cortex-monitor.sh
     mv ./cortex-package/service/cortex-monitor.sh /etc/init.d/cortex-monitor.sh
+    mv ./cortex-package/cortexnode.conf /etc/supervisor/conf.d/cortexnode.conf
+
+    mv ./cortex-package/* $DPLOY_PATH/
+    chmod +x $DPLOY_PATH/cortex
     update-rc.d cortex-monitor.sh defaults
-    mv ./cortex-package/supervisor-config/cortexnode.conf /etc/supervisor/conf.d/cortexnode.conf
 
     supervisorctl reload
     sleep 5
@@ -136,7 +84,6 @@ helpdoc() {
 
     echo "Most used commands:"
     echo "  deploy - download and deploy cortex fullnode"
-    echo "  install - deploy cortex fullnode from existing file"
     echo "  clean - clean the current cortex environment"
 }
 
@@ -156,14 +103,14 @@ case "$1" in
     'install')
         deploy
         ;;
-        
+
     'clean')
         clean
         echo 'Try to remove existing cortex chain data'
         rm -rf /root/.cortex
         rm -rf $HOME/.cortex
         ;;
-        
+
     *)
         helpdoc
         exit 1
